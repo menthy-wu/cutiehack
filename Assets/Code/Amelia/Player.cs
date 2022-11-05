d0d6c9c4e7a2fc05f912751ab3c6f0275bda94eb
andersAsch0 <84699083+andersAsch0@users.noreply.github.com>
2022-11-05 13:19:12 -0700

    player can move
    player velocity moved with w key

--- Assets/Code/Amelia/Player.cs
+++ Assets/Code/Amelia/Player.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>(); // game object is thing that script is attatched to
+
+        Debug.Log("testing");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.W)){
+            rb.velocity = new Vector2(0, 5);
+        }
     }
 }
